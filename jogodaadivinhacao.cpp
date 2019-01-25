cb0566d09fad88e0d6a72313fe8c2b0c2960acb8
Daniel Mendes <daniel@daniel.sp01>
2019-01-25 15:58:25 -0200

    Entrada e saída de dados.

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "************************************" << std::endl;
-    std::cout << "* Bem vindo ao jogo da adivinhação *" << std::endl;
-    std::cout << "************************************" << std::endl;
+    cout << "*************************************" << endl;
+    cout << "* Bem vindo ao jogo da adivinhação! *" << endl;
+    cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+    int chute = 0;
+
+    cout << "Qual seu chute? ";
+
+    cin >> chute;
+
+    cout << "O valor do seu chute é " << chute << endl;
+
+    if(chute == numero_secreto) {
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }
+    else if(chute > numero_secreto) {
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
+
     return 0;
 }
