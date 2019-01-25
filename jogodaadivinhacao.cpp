09edde5ffd2002d941c91685c9309d3aa4ef64a3
Daniel Mendes <daniel@daniel.sp01>
2019-01-25 15:35:43 -0200

    Primeira função do curso.

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "************************************" << std::endl;
+    std::cout << "* Bem vindo ao jogo da adivinhação *" << std::endl;
+    std::cout << "************************************" << std::endl;
+    return 0;
+}
